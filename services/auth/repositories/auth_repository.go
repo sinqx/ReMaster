44e622498c5195effb1b2c653c8145a7b26e31e2
sinqx <sinqxpls@gmail.com>
2025-09-10 19:00:32 +0600

    some new funcs

--- services/auth/repositories/auth_repository.go
+++ services/auth/repositories/auth_repository.go
@@ -2,14 +2,18 @@ package repositories
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"time"
 
 	models "remaster/services/auth/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// TODO: refactor errors
 const (
 	UsersCollection         = "users"
 	RefreshTokensCollection = "refresh_tokens"
@@ -17,14 +21,16 @@ const (
 )
 
 type AuthRepository struct {
-	collection *mongo.Collection
-	logger     *slog.Logger
+	collection              *mongo.Collection
+	refreshTokensCollection *mongo.Collection
+	logger                  *slog.Logger
 }
 
 func NewAuthRepository(db *mongo.Database, logger *slog.Logger) *AuthRepository {
 	return &AuthRepository{
-		collection: db.Collection("users"),
-		logger:     logger,
+		collection:              db.Collection("users"),
+		refreshTokensCollection: db.Collection("refresh_tokens"),
+		logger:                  logger.With(slog.String("repository", "auth")),
 	}
 }
 
@@ -54,11 +60,54 @@ type AuthRepositoryInterface interface {
 	CreateIndexes(ctx context.Context) error
 }
 
-func (r *AuthRepository) CreateUser(ctx context.Context, user *models.User) error {
-	_, err := r.collection.InsertOne(ctx, user)
+func (r *AuthRepository) Create(ctx context.Context, user *models.User) error {
+	user.BeforeCreate()
+
+	count, err := r.collection.CountDocuments(ctx, bson.M{"email": user.Email})
 	if err != nil {
-		r.logger.Error("failed to create user in db", "error", err, "email", user.Email)
-		return err
+		return fmt.Errorf("failed to check email uniqueness: %w", err)
+	}
+
+	if count > 0 {
+		return fmt.Errorf("user with email %s already exists", user.Email)
+	}
+
+	_, err = r.collection.InsertOne(ctx, user)
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
 	}
 	return nil
+}
+
+func (r *AuthRepository) GetByEmail(ctx context.Context, email string) (*models.User, error) {
+	var user models.User
+	err := r.collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, fmt.Errorf("failed to get user by email: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (r *AuthRepository) SaveRefreshToken(ctx context.Context, token *models.RefreshToken) error {
+	opts := options.Update().SetUpsert(true)
+	filter := bson.M{"user_id": token.UserID}
+	update := bson.M{"$set": token}
+
+	_, err := r.refreshTokensCollection.UpdateOne(ctx, filter, update, opts)
+	return err
+}
+
+func isUniqueConstraintError(err error) bool {
+	if writeException, ok := err.(mongo.WriteException); ok {
+		for _, writeError := range writeException.WriteErrors {
+			if writeError.Code == 11000 { // 11000 - duplication error code MongoDB
+				return true
+			}
+		}
+	}
+	return false
 }
